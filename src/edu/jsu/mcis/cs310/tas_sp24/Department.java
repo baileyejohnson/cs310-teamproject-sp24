e06643a5ee9cad35fc2a6c8d19f1b18672afb8dc
IBxTimmy <timi.tella@outlook.com>
2024-02-22 18:33:56 -0600

    Department.java added to

--- src/edu/jsu/mcis/cs310/tas_sp24/Department.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/Department.java
@@ -8,6 +8,31 @@ package edu.jsu.mcis.cs310.tas_sp24;
  *
  * @author egmck
  */
-class Department {
+public class Department {
+    private final String id, description;
+    private final int termid;
+
+    public Department(String id, String description, int termid) {
+        this.id = id;
+        this.description = description;
+        this.termid = termid;
+    }
+    public String getId(){
+        return id;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public int getTermid(){
+        return termid;
+    }
     
+    @Override
+    public String toString(){
+        StringBuilder dpString = new StringBuilder();
+        dpString.append('#').append(id).append(' ');
+        dpString.append('(').append(description).append(')');
+        dpString.append('#').append(termid).append(')');
+        return dpString.toString();
+    }
 }
