7446f96d45c93181d8a6635cbf6d773068bd301c
Eli <emckee1@stu.jsu.edu>
2024-02-27 21:26:15 -0600

    Eli got the first find() working and need errors for second find() working

--- src/edu/jsu/mcis/cs310/tas_sp24/dao/EmployeeDAO.java
+++ src/edu/jsu/mcis/cs310/tas_sp24/dao/EmployeeDAO.java
@@ -4,10 +4,143 @@
  */
 package edu.jsu.mcis.cs310.tas_sp24.dao;
 
+import edu.jsu.mcis.cs310.tas_sp24.Badge;
+import edu.jsu.mcis.cs310.tas_sp24.Employee;
+
+import java.sql.*;
+
+import java.sql.SQLException;
 /**
  *
  * @author TIMI
  */
 public class EmployeeDAO {
+    private final DAOFactory daoFactory;
     
+    private static final String QUERY_FIND = "SELECT * FROM department WHERE id = ?";
+
+    EmployeeDAO(DAOFactory daoFactory) {
+
+        this.daoFactory = daoFactory;
+}
+    public Employee find(int id){
+         Employee emp = null;
+
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+
+            Connection conn = daoFactory.getConnection();
+
+            if (conn.isValid(0)) {
+
+                ps = conn.prepareStatement(QUERY_FIND);
+                ps.setInt(1, id);
+
+                boolean hasresults = ps.execute();
+
+                if (hasresults) {
+
+                    rs = ps.getResultSet();
+
+                    while (rs.next()) {
+                        String description = rs.getString("description");
+                        emp = new Employee(id);
+
+                    }
+
+                }
+
+            }
+
+        }
+         
+         catch (SQLException e) {
+
+            throw new DAOException(e.getMessage());
+
+        } finally {
+
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+
+        }
+        
+         return emp;
+}
+     
+    public Employee find(Badge id){
+        //otherBadge.getid
+        Employee emp = null;
+
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+
+            Connection conn = daoFactory.getConnection();
+
+            if (conn.isValid(0)) {
+
+                ps = conn.prepareStatement(QUERY_FIND);
+                ps.setString(1, id);
+                  
+
+                boolean hasresults = ps.execute();
+
+                if (hasresults) {
+
+                    rs = ps.getResultSet();
+
+                    while (rs.next()) {
+                        String description = rs.getString("description");
+                        badge = new Employee(id);
+
+                    }
+
+                }
+
+            }
+
+        }
+         
+         catch (SQLException e) {
+
+            throw new DAOException(e.getMessage());
+
+        } finally {
+
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+
+        }
+        
+         return emp;
+         //Should be returning Badge id instead of Employee object
+}
 }
